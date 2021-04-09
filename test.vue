671b492e094f4509ae068d835a970ba60231b323
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-04-09 21:09:45 +0500

    Render valid and fetch tickerName

---
+++ test.vue
@@ -0,0 +1,33 @@
+<template>
+  <ol>
+    <li v-for="(item, idx) in items" :key="idx">
+      {{ item.name }}: {{ item.price }}
+    </li>
+  </ol>
+  <button @click="add">Add</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: "Lorem1", price: 123 },
+        { name: "Lorem2", price: 345 },
+        { name: "Lorem3", price: 456 },
+      ],
+    };
+  },
+  methods: {
+    add() {
+      const newItem = { name: "Lorem4", price: 279 };
+      setTimeout(() => {
+        this.items.push(newItem);
+      }, 3000);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
